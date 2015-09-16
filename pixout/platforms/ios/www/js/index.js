7571736c8cdeeeac99271cc5b8e5241649a9bcd8
jagrin <jagrin01@gmail.com>
2015-09-16 18:32:07 +0200

    map now working

--- pixout/platforms/ios/www/js/index.js
+++ pixout/platforms/ios/www/js/index.js
@@ -32,8 +32,9 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
+        initMap();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
--- pixout/platforms/windows/www/js/pixoutmap.js
+++ pixout/platforms/windows/www/js/pixoutmap.js
@@ -1,4 +1,4 @@
-var map;
+﻿var map;
 var icon;
 var linedata;
 var line;
