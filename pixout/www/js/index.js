5db51e50a12f849ca2b22896953f69aa6ddb2707
Diliz <dilizmegameuh@gmail.com>
2015-09-16 23:56:45 +0200

    ajout de la prise de photo sur android et set la photo dans une balise img une fois sauvegardée, manque l'ajout de la position ou a été prise la photo géographiquement, j'y travaille

--- pixout/www/js/index.js
+++ pixout/www/js/index.js
@@ -1,3 +1,32 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
 function onLoad() {
     local();
+}
+
+function onDeviceReady() {
+    //console.log("ready");
+}
+
+function takePicture() {
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality : 75,
+    destinationType : Camera.DestinationType.DATA_URL,
+    sourceType : Camera.PictureSourceType.CAMERA,
+    allowEdit : true,
+    encodingType: Camera.EncodingType.JPEG,
+    targetWidth: 100,
+    targetHeight: 100,
+    popoverOptions: CameraPopoverOptions,
+    saveToPhotoAlbum: true
+  });
+}
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
 }
